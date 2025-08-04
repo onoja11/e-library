ea720474f1a155dc01b2ac22c421c71f397e2f77
Veritas <ochigbogodswill868@gmail.com>
2025-08-04 16:31:33 -0700

    changed to bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,12 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <link rel="shortcut icon" href="{{ asset('logo.png') }}" type="image/x-icon">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <!-- Scripts -->
+  <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-qJYIGmJq5nN9F5nUgVYf1tPjVqzzAcB4y1UbZ3dV5DgKqlFgTjj6NIE1n4I0ShmF" 
+        crossorigin="anonymous">
+            <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/style.css'])
 </head>
 <body >
@@ -30,5 +34,10 @@
         @include('partials.footer')
         @include('sweetalert::alert')
     </div>
+       <script 
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-rUgV4nDp8Y2FNRK5hJDfS5Y5e0bfl4N1U3E9uvcgX9hj2i2qSo4TyfZB2V7xH3Jt" 
+        crossorigin="anonymous">
+    </script>
 </body>
 </html>
